2c40f7151a09dcd90e290694b42fb18380bcb612
IT20207854 <maleeshadesilva21@gmail.com>
2022-05-10 13:56:39 +0530

    Insert, Update Complete

--- PowerGenerators-Part02/src/com/PowerGeneratorsAPI.java
+++ PowerGenerators-Part02/src/com/PowerGeneratorsAPI.java
@@ -1,4 +1,5 @@
 package com;
+import com.PowerGenerator;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
@@ -6,6 +7,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException; 
+import java.util.HashMap; 
+import java.util.Map; 
+import java.util.Scanner;
 
 /**
  * Servlet implementation class PowerGeneratorsAPI
@@ -13,7 +18,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/PowerGeneratorsAPI")
 public class PowerGeneratorsAPI extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+    PowerGenerator pGen = new PowerGenerator();
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -33,23 +38,62 @@ public class PowerGeneratorsAPI extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	/*protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{ 
+		String output = pGen.addPowerGenerator(request.getParameter("gCode"), 
+		request.getParameter("gName"), 
+		request.getParameter("gType"), 
+		request.getParameter("gLocation"),
+		request.getParameter("gUnitPrice"),
+		request.getParameter("gRegDate")); 
+		response.getWriter().write(output); 
+	}*/
+	
+	
+	// Convert request parameters to a Map
+	private static Map getParasMap(HttpServletRequest request) 
+	{ 
+		Map<String, String> map = new HashMap<String, String>(); 
+		try
+		{ 
+			 Scanner scanner = new Scanner(request.getInputStream(), "UTF-8"); 
+			 String queryString = scanner.hasNext() ? 
+			 scanner.useDelimiter("\\A").next() : ""; 
+			 scanner.close(); 
+			 String[] params = queryString.split("&"); 
+			 for (String param : params) 
+			 { 
+				 String[] p = param.split("="); 
+				 map.put(p[0], p[1]); 
+			 } 
+		} 
+	    catch (Exception e) 
+	    { } 
+	
+		return map; 
 	}
 
-	/**
-	 * @see HttpServlet#doPut(HttpServletRequest, HttpServletResponse)
-	 */
-	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+
+	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{ 
+		Map paras = getParasMap(request); 
+		String output = pGen.updatePowerGenerator(paras.get("hidgIDSave").toString(), 
+		paras.get("gCode").toString(), 
+		paras.get("gName").toString(), 
+		paras.get("gType").toString(), 
+		paras.get("gLocation").toString(),
+		paras.get("gUnitPrice").toString(),
+		paras.get("gRegDate").toString()); 
+		response.getWriter().write(output); 
+	} 
+	
+	
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{ 
+		Map paras = getParasMap(request); 
+		String output = pGen.deletePowerGenerator(paras.get("gID").toString()); 
+		response.getWriter().write(output); 
 	}
 
-	/**
-	 * @see HttpServlet#doDelete(HttpServletRequest, HttpServletResponse)
-	 */
-	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-	}
 
 }
